57cb96600dce76a27eddb954e11b496d9649fec0
Awie van der Merwe <110628659+AwievdM99@users.noreply.github.com>
2022-08-30 22:47:37 +0200

    First Push
    First push

---
+++ Awie van der Merwe_PRG381_Project/Phase 1/2/Awie-van-der-Merwe_PRG381_Project/src/main/java/com/example/Awie/van/der/Merwe_PRG381_Project/AwieVanDerMerwePrg381ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Awie.van.der.Merwe_PRG381_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwieVanDerMerwePrg381ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwieVanDerMerwePrg381ProjectApplication.class, args);
+	}
+
+}
---
+++ Awie van der Merwe_PRG381_Project/Phase 1/2/Awie-van-der-Merwe_PRG381_Project/src/test/java/com/example/Awie/van/der/Merwe_PRG381_Project/AwieVanDerMerwePrg381ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Awie.van.der.Merwe_PRG381_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwieVanDerMerwePrg381ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
